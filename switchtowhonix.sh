1871a010a0be261644bb59ee21de1b092c70cf32
syk0path <70035442+syk0path@users.noreply.github.com>
2023-01-23 16:57:21 +0100

    Update switchtowhonix.sh

--- switchtowhonix.sh
+++ switchtowhonix.sh
@@ -2,10 +2,10 @@
 
 # function to display help message
 display_help() {
-    echo -e "Usage: $0 [\033[32madd\033[0m|\033[31mdelete\033[0m] [--help] [\033[34mdns\033[0m]"
-    echo -e "\033[32madd\033[0m: adds the Tor Whonix configuration to the interfaces and resolv.conf files"
-    echo -e "\033[31mdelete\033[0m: deletes the Tor Whonix configuration from the interfaces and resolv.conf files and replaces nameserver 10.152.152.10 with 9.9.9.9 or provided dns"
-    echo -e "\033[34mdns\033[0m: use this option to provide dns server to be used instead of 9.9.9.9"
+    echo "Usage: $0 [add|delete] [--help] [dns]"
+    echo "add: adds the Tor Whonix configuration to the interfaces and resolv.conf files"
+    echo "delete: deletes the Tor Whonix configuration from the interfaces and resolv.conf files and replaces nameserver 10.152.152.10 with 9.9.9.9 or provided dns"
+    echo "dns: use this option to provide dns server to be used instead of 9.9.9.9"
     echo "--help: displays this help message"
 }
 
@@ -28,11 +28,6 @@ if [ "$1" == "add" ]; then
         echo "Error: Tor Whonix configuration already exists in interfaces file"
         exit 1
     else
-        # check if tor and torbrowser is installed or not
-        if ! dpkg -s tor &> /dev/null; then
-            sudo apt update -y && sudo apt upgrade -y
-            sudo apt install -y tor torbrowser-launcher
-        fi
         # add the configuration to the interfaces file
         sudo bash -c 'echo "#Tor Whonix" >> /etc/network/interfaces'
         sudo bash -c 'echo "auto eth0" >> /etc/network/interfaces'
@@ -41,12 +36,6 @@ if [ "$1" == "add" ]; then
         sudo bash -c 'echo "netmask 255.255.192.0" >> /etc/network/interfaces'
         sudo bash -c 'echo "gateway 10.152.152.10" >> /etc/network/interfaces'
         echo "Tor Whonix configuration added to interfaces file"
-        # Starting Tor Service
-        if ! systemctl is-active --quiet tor; then
-            sudo systemctl tor start
-        else
-            echo "Tor service is already up and running"
-        fi
     fi
     if grep -q "nameserver 10.152.152.10" /etc/resolv.conf; then
         echo "Error: nameserver 10.152.152.10 already exists in resolv.conf file"
@@ -65,22 +54,16 @@ elif [ "$1" == "delete" ]; then
     # delete the configuration from the interfaces file
     sudo sed -i '/#Tor Whonix/,+5d' /etc/network/interfaces
     echo "Tor Whonix configuration deleted from interfaces file"
-    #Stopping Tor Service
-    sudo systemctl tor stop
     # replace nameserver 10.152.152.10 with 9.9.9.9 or provided dns in the resolv.conf file
     if [ $# -lt 3 ]; then
     sudo sed -i 's/nameserver 10.152.152.10/nameserver 9.9.9.9/g' /etc/resolv.conf
     echo "nameserver 10.152.152.10 replaced with 9.9.9.9 in resolv.conf file"
-    else
+else
     sudo sed -i "s/nameserver 10.152.152.10/nameserver $3/g" /etc/resolv.conf
     echo "nameserver 10.152.152.10 replaced with $3 in resolv.conf file"
-    fi
+fi
 else
     echo "Error: invalid argument provided"
     display_help
     exit 1
-fi
-
-if [ "$1" == "add" ]; then
-    echo "Please remember to change the network settings of Virtualbox to Internal Network > Whonix"
 fi
